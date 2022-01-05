df9f7f7149630a50a0d3c008f973647b38cc4981
jenny <jenny@MBP-de-Jenny.localdomain>
2022-01-05 15:38:25 +0100

    add route for playlist

--- controllers/PlaylistController.js
+++ controllers/PlaylistController.js
@@ -3,14 +3,49 @@ const Playlist = require("../models/Playlist");
 
 const router = express.Router();
 
+// get Playlists by User /user/:user_id
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // create Playlist
+router.post("/", async (req, res) => {
+  try {
+    // code here
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-// get Playlists by User 
+// get Tracks from Playlist
+router.get("/:id/tracks", async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-// get Tracks Playlist
-
-// add tracks to Playlist 
+// add tracks to Playlist
+router.post("/:id/track", async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // delete tracks from Playlist
+router.delete("/:id/track", async (req, res) => {
+  try {
+    // code here
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
