6cebe2fa79ff5e5bc88c007fd179763845fd2936
jenny <jenny@MBP-de-Jenny.localdomain>
2022-01-05 15:36:34 +0100

    add routing playlist

--- routes/routing.js
+++ routes/routing.js
@@ -1,7 +1,9 @@
 const AlbumController = require('../controllers/AlbumController.js');
 const GenderController = require('../controllers/GenderController.js');
+const PlaylistController = require('../controllers/PlaylistController.js');
 
 module.exports = (app) => {
     app.use('/albums', AlbumController);
     app.use('/genders', GenderController);
+    app.use('/playlists', PlaylistController);
 }
